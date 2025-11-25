3f3501a6e9423421c47ad4fb242264d7dd1159a9
s-t-e-v <steven.bandaogo@proton.me>
2025-11-25 20:16:57 +0100

    ex00 v2

--- module05/ex00/matrix.py
+++ module05/ex00/matrix.py
@@ -1,34 +1,38 @@
+import sys
+
 class Matrix:
 
-    def __init__(self, data, shape):
-        self.data = data
-        self.shape = shape
-
-    @classmethod
-    def from_data(cls, data):
-        """Initialize the maxtrix from a list of lists"""
-        # TODO - add a check for data
-        shape = (len(data), len(data[0]))
-        return cls(data, shape)
-
-    @classmethod
-    def from_shape(cls, shape):
-        """Initialize the matrix from shape with zeroes"""
-        # TODO - add a check for shape
-        data = [[0 for j in range(shape[0])] for i in range(shape[1])]
-        return cls(data, shape)
-
+    def __init__(self, arg):
+        if isinstance(arg, (int, float)):
+            self.shape = arg
+            self.data = [[0 for j in range(self.shape[0])] for i in range(self.shape[1])]
+            return
+        if isinstance(arg, list):
+            if all(isinstance(elem, int) for elem in arg):
+                m = 1
+                n = len(arg)
+            elif all(isinstance(elem, list) and len(elem) == len(arg[0]) for elem in arg):
+                m = len(arg)
+                n = len(arg[0])
+            else:
+                print(f"Error: Invalid matrix data: {arg}", file=sys.stderr)
+                raise NotImplemented
+            self.shape = (m, n)
+            self.data = arg
+            return
+        raise NotImplemented
+    
     # add : only matrices of same dimensions.
     def __add__(self, other):
         if not isinstance(other, Matrix):
             raise NotImplemented
         if self.shape != other.shape:
             raise NotImplemented
-        return Matrix.from_data(
+        return Matrix(
             [[val_a + val_b for val_a, val_b in zip(row_a, row_b)] for row_a, row_b in zip(self.data, other.data)]
         )
         
-    # __radd__ = __add__
+    __radd__ = __add__
 
     # sub : only matrices of same dimensions.
     def __sub__(self, other):
@@ -36,7 +40,7 @@ class Matrix:
             raise NotImplemented
         if self.shape != other.shape:
             raise NotImplemented
-        return Matrix.from_data(
+        return Matrix(
             [[val_a - val_b for val_a, val_b in zip(row_a, row_b)] for row_a, row_b in zip(self.data, other.data)]
         )
 
@@ -48,14 +52,14 @@ class Matrix:
             raise NotImplemented
         if scalar == 0:
             raise ZeroDivisionError
-        return Matrix.from_data(
+        return Matrix(
             [[v / scalar for v in row] for row in self.data]
         )
     
     def __rtruediv__(self, scalar):
         if not isinstance(scalar, (int, float)):
             raise NotImplemented
-        return Matrix.from_data(
+        return Matrix(
             [[scalar / v for v in row] for row in self.data]
         )
 
@@ -64,7 +68,7 @@ class Matrix:
     # returns a Vector if we perform Matrix * Vector mutliplication.
     def __mul__(self, other):
         if isinstance(other, (int, float)):
-            return Matrix.from_data(
+            return Matrix(
                 [[v * other for v in row] for row in self.data]
             )
         if isinstance(other, Vector):
@@ -81,15 +85,42 @@ class Matrix:
                         new_val += val_a * other.data[i][j]
                     new_row.append(new_val)
                 data.append(new_row)
-            return Matrix.from_data(data)
+            return Matrix(data)
         raise NotImplemented
 
         
 
     # __rmul__
-    # __str__
-    # __repr__
+
+    def T(self):
+        if self.shape[1] == 1:
+            return Matrix([v[0] for v in self.data])
+        return Matrix([[row[i] for row in self.data] for i in range(self.shape[1])])
+
+    def __str__(self):
+        return f"{self.data}"
+    
+    def __repr__(self):
+        return f"Matrix(data={self.data}, shape={self.shape})"
 
 class Vector(Matrix):
-    def __init__(self, data, shape):
-        super().__init__(data, shape)
+    def __init__(self, arg):
+        if not isinstance(arg, list):
+            raise NotImplemented
+        if all(isinstance(v, int) for v in arg):
+            super().__init__(arg)
+            return
+        elif all(isinstance(elem, list) and len(elem) == 1 for elem in arg):
+            super().__init__(arg)
+            return
+        raise NotImplemented
+            
+    def dot(self, v):
+        if not isinstance(v, Vector):
+            raise NotImplemented
+        if self.shape[1] != v.shape[0]:
+            raise NotImplemented
+        if self.shape[0] == 1:
+            return sum(va * vb for va, vb in zip(self.data, v.T().data))
+        return sum(va * vb for va, vb in zip(self.T().data, v.data))
+    
--- module05/ex00/test.py
+++ module05/ex00/test.py
@@ -1,6 +1,6 @@
-from matrix import Matrix
+from matrix import Matrix, Vector
 
-A = Matrix.from_data(
+A = Matrix(
     [
         [1, 2],
         [3, 4]
@@ -9,7 +9,7 @@ A = Matrix.from_data(
 
 B = A
 
-C = Matrix.from_data(
+C = Matrix(
     [
         [0, 1],
         [1, 1]
@@ -20,7 +20,40 @@ res1 = A + A - C
 # res2 = B + C
 print(f"{res1.data}, shape: {res1.shape}")
 # print(f"{res2.data}, shape: {res2.shape}")
-print((A/2).data)
-print((2/A).data)
-print((A * 2).data)
-print((A * C).data)
+print(A/2)
+print(2/A)
+print(A * 2)
+print(A * C)
+print(repr(A))
+print(A.T())
+v1 = Vector(
+    [1, 2, 3]
+)
+print(v1)
+v2 = Vector(
+    [
+        [1],
+        [2],
+        [3]
+    ]
+)
+print(repr(v2))
+print(f"v2.T(): {v2.T()}")
+print(f"v1.v2 = {v1.dot(v2)}")
+print(f"v2.v1 = {v2.dot(v1)}")
+E = Matrix(
+    [1, 2, 3]
+)
+print(repr(E))
+
+F = Matrix(
+    [
+        [1, 10],
+        [2, 20],
+        [3, 30],
+        [4, 40],
+        [5, 50]
+    ]
+)
+print(repr(F))
+print(v1 + v1)
