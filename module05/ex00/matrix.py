cf401f145ae25b6a2fad58104de1cea63ab34b78
s-t-e-v <steven.bandaogo@proton.me>
2025-11-27 01:23:11 +0100

    ex00 v3

--- module05/ex00/matrix.py
+++ module05/ex00/matrix.py
@@ -5,96 +5,116 @@ class Matrix:
     def __init__(self, arg):
         if isinstance(arg, (int, float)):
             self.shape = arg
-            self.data = [[0 for j in range(self.shape[0])] for i in range(self.shape[1])]
+            self.data = [
+                [0 for j in range(self.shape[0])]
+                for i in range(self.shape[1])
+                ]
             return
         if isinstance(arg, list):
-            if all(isinstance(elem, int) for elem in arg):
+            if all(isinstance(elem, (int, float)) for elem in arg):
                 m = 1
                 n = len(arg)
-            elif all(isinstance(elem, list) and len(elem) == len(arg[0]) for elem in arg):
+            elif all(isinstance(elem, list) and len(elem) == len(arg[0])
+                     for elem in arg):
                 m = len(arg)
                 n = len(arg[0])
             else:
                 print(f"Error: Invalid matrix data: {arg}", file=sys.stderr)
-                raise NotImplemented
+                raise NotImplementedError
             self.shape = (m, n)
             self.data = arg
             return
-        raise NotImplemented
+        raise NotImplementedError
     
     # add : only matrices of same dimensions.
     def __add__(self, other):
         if not isinstance(other, Matrix):
-            raise NotImplemented
+            raise NotImplementedError
         if self.shape != other.shape:
-            raise NotImplemented
-        return Matrix(
-            [[val_a + val_b for val_a, val_b in zip(row_a, row_b)] for row_a, row_b in zip(self.data, other.data)]
-        )
+            raise NotImplementedError
+        return Matrix([
+            [val_a + val_b for val_a, val_b in zip(row_a, row_b)]
+            if isinstance(row_a, list) and isinstance(row_b, list)
+            else row_a + row_a
+            for row_a, row_b in zip(self.data, other.data)
+            ])
         
     __radd__ = __add__
 
     # sub : only matrices of same dimensions.
     def __sub__(self, other):
         if not isinstance(other, Matrix):
-            raise NotImplemented
+            raise NotImplementedError
         if self.shape != other.shape:
-            raise NotImplemented
-        return Matrix(
-            [[val_a - val_b for val_a, val_b in zip(row_a, row_b)] for row_a, row_b in zip(self.data, other.data)]
-        )
+            raise NotImplementedError
+        return Matrix([
+            [val_a - val_b for val_a, val_b in zip(row_a, row_b)]
+            if isinstance(row_a, list) and isinstance(row_b, list)
+            else row_a - row_b
+            for row_a, row_b in zip(self.data, other.data)
+            ])
 
     __rsub__ = __sub__
     
     # div : only scalars.
     def __truediv__(self, scalar):
         if not isinstance(scalar, (int, float)):
-            raise NotImplemented
+            raise NotImplementedError
         if scalar == 0:
             raise ZeroDivisionError
-        return Matrix(
-            [[v / scalar for v in row] for row in self.data]
-        )
+        return Matrix([
+            [v / scalar for v in row]
+            if isinstance(row, list)
+            else row / scalar
+            for row in self.data
+            ])
     
     def __rtruediv__(self, scalar):
         if not isinstance(scalar, (int, float)):
-            raise NotImplemented
-        return Matrix(
-            [[scalar / v for v in row] for row in self.data]
-        )
-
+            raise NotImplementedError
+        return Matrix([
+            [scalar / v for v in row]
+            if isinstance(row, list)
+            else scalar / row
+            for row in self.data
+            ])
 
     # mul : scalars, vectors and matrices , can have errors with vectors and matrices,
     # returns a Vector if we perform Matrix * Vector mutliplication.
     def __mul__(self, other):
         if isinstance(other, (int, float)):
-            return Matrix(
-                [[v * other for v in row] for row in self.data]
-            )
-        if isinstance(other, Vector):
-            pass
-        if isinstance(other, Matrix):
+            data = [[v * other for v in row] for row in self.data]
+        elif isinstance(other, Matrix):
             if self.shape[1] != other.shape[0]:
                 raise ArithmeticError
             data = []
-            for row_a in self.data:
+            for m in range(self.shape[0]):
+                row = self.data if self.shape[0] == 1 else self.data[m]
                 new_row = []
-                for j in range(other.shape[1]):
-                    new_val = 0
-                    for i, val_a in enumerate(row_a):
-                        new_val += val_a * other.data[i][j]
-                    new_row.append(new_val)
-                data.append(new_row)
-            return Matrix(data)
-        raise NotImplemented
+                for p in range(other.shape[1]):
+                    new_val = sum(val * other.data[n][p] if other.shape[0] > 1 else val * other.data[p] for val, n in zip(row, range(other.shape[0])))
+                    new_row.append(new_val) 
+                if self.shape[0] > 1:
+                    data.append(new_row)
+                else:
+                    data = new_row
+                    break
+        else:
+            raise NotImplementedError
+        if Vector.one_dimension(data) and \
+            (isinstance(self, Vector) or isinstance(other, Vector)):
+            return Vector(data)
+        return Matrix(data)
 
         
+    __rmul__ = __mul__
 
-    # __rmul__
 
     def T(self):
         if self.shape[1] == 1:
             return Matrix([v[0] for v in self.data])
+        if self.shape[0] == 1:
+            return Matrix([[v] for v in self.data])
         return Matrix([[row[i] for row in self.data] for i in range(self.shape[1])])
 
     def __str__(self):
@@ -106,21 +126,22 @@ class Matrix:
 class Vector(Matrix):
     def __init__(self, arg):
         if not isinstance(arg, list):
-            raise NotImplemented
-        if all(isinstance(v, int) for v in arg):
-            super().__init__(arg)
-            return
-        elif all(isinstance(elem, list) and len(elem) == 1 for elem in arg):
-            super().__init__(arg)
-            return
-        raise NotImplemented
-            
+            raise NotImplementedError
+        if not Vector.one_dimension(arg):
+            raise NotImplementedError
+        super().__init__(arg)
+    
     def dot(self, v):
         if not isinstance(v, Vector):
-            raise NotImplemented
+            raise NotImplementedError
         if self.shape[1] != v.shape[0]:
-            raise NotImplemented
+            raise NotImplementedError
         if self.shape[0] == 1:
             return sum(va * vb for va, vb in zip(self.data, v.T().data))
         return sum(va * vb for va, vb in zip(self.T().data, v.data))
+    
+    @staticmethod
+    def one_dimension(data):
+        return  all(isinstance(v, (int, float)) for v in data) or \
+                all(isinstance(elem, list) and len(elem) == 1 for elem in data)
     
--- module05/ex00/test.py
+++ module05/ex00/test.py
@@ -56,4 +56,68 @@ F = Matrix(
     ]
 )
 print(repr(F))
-print(v1 + v1)
+v3 = v2 + v2
+print(f"type(v3): {type(v3), {repr(v3)}}")
+v4 = v1 + v1
+print(f"type(v4): {type(v4), {repr(v4)}}")
+
+v10 = Matrix(
+    [10, 20, 30]
+)
+v20 = Matrix(
+    [
+        [10],
+        [20],
+        [30]
+    ]
+)
+
+v5 = v10 - v1
+print(f"type(v5): {type(v5), {repr(v5)}}")
+v6 = v20 - v2
+print(f"type(v6): {type(v6), {repr(v6)}}")
+
+print(f"v1 / 2 = {v1 / 2}")
+print(f"v2 / 2 = {v2 / 2}")
+print(f"2 / v1 = {2 / v1}")
+print(f"2 / v2 = {2 / v2}")
+
+print(f"T(v1): {v1.T()}")
+print(f"T(v2): {v2.T()}")
+print(f"T(T(v1)): {v1.T().T()}")
+print(f"T(F): {F.T()}")
+print(f"T(T(F)): {F.T().T()}")
+
+G = Matrix(
+    [1, 2, 3]
+)
+
+H = Matrix(
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ]
+)
+
+I = G * H
+print(f"G * H = {I}")
+J = Matrix(
+    [
+        [1],
+        [2],
+        [3]
+    ]
+)
+K = H * J
+print(f"H * J = {K}")
+L = G * J
+print(f"G * J = {L}")
+M = J * G
+print(f"J * G = {M}")
+
+print(f"2 * J = { 2 * J}")
+print(f"J * 2 = { J * 2}")
+
+print(f"2 * G = { 2 * G}")
+print(f"G * 2 = { G * 2}")
