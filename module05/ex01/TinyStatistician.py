ea9d2b6377d4bd4a7d3257cd0c105210895be52a
s-t-e-v <steven.bandaogo@proton.me>
2025-11-28 19:24:21 +0100

    ex01 v2: refac mean + median done

--- module05/ex01/TinyStatistician.py
+++ module05/ex01/TinyStatistician.py
@@ -3,8 +3,32 @@ import numpy as np
 
 class TinyStatistician:
     def mean(x):
+        x = TinyStatistician.validate_input(x)
+        if not isinstance(x, np.ndarray):
+            return None
+        return sum(x) / x.shape[0]
+
+    def median(x):
+        x = TinyStatistician.validate_input(x)
+        if not isinstance(x, np.ndarray):
+            return None
+        x.sort()
+        m = x.shape[0]
+        n = m // 2
+        return float(x[n] if m % 2 != 0 else (x[n - 1] + x[n]) / 2)
+
+    @staticmethod
+    def validate_input(x):
         if isinstance(x, list):
             x = np.ndarray(x)
         if not isinstance(x, np.ndarray):
             return None
-        return sum(x) / x.shape[0]
+        if not (
+            len(x.shape) == 1 or
+                (len(x.shape) == 2 and x.shape[1] == 1)):
+            return None
+        if len(x.shape) == 2 and x.shape[1] == 1:
+            x = x.T[0]
+        if not all(np.isreal(x)):
+            return None
+        return x
--- module05/ex01/test.py
+++ module05/ex01/test.py
@@ -2,6 +2,28 @@ from TinyStatistician import TinyStatistician
 import numpy as np
 
 x1 = np.array([1, 2, 3, 4, 5])
+x2 = np.array([
+        [1],
+        [2],
+        [3],
+        [4],
+        [5]
+    ])
 
 x1_bar = TinyStatistician.mean(x1)
 print(f"x1 = {x1}, x1_bar = {x1_bar}")
+
+x2_bar = TinyStatistician.mean(x2)
+print(f"x2 = {x2}, x2_bar = {x2_bar}")
+print(TinyStatistician.mean(np.array([[10]])))
+
+print(TinyStatistician.median(x1))
+
+x3 = np.array([2, 3, 2, 5, 9, 1])
+
+x4 = np.array(
+    [4, 5, 1, 0, 10]
+)
+
+print(f"median(x3) = {TinyStatistician.median(x3)}")
+print(f"median(x4) = {TinyStatistician.median(x4)}")
